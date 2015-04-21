2704861e32c9ff30d93653639b77d3d04c547739
phale01 <patriciahale@MacBook-Pro.local>
2015-04-21 08:31:54 -0400

    Renamed appropriately, more edits
    I may have worked out some bugs, and tried to clean up our files to
    find everything easier

--- Message Decoder/willsdecoding.m
+++ Message Decoder/willsdecoding.m
@@ -2,10 +2,11 @@ clear
 clc
 
 
-message = 'hellohappy';
+message = 'GSV XOZHH RMGILWFXGRLM GL VMTRMVVIRMT SZH YVVM JFRGV ZM RMGVIVHGRMT XOZHH.' ;
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz';
 letterFreq = zeros(1, length(alphabet));
+alphCommon = 'etaoinshrdlcumwfgybpvkjxqz';
 
 for i = 1:length(message)
     dataCells{1, i} = message(i);
@@ -15,9 +16,18 @@ for i = 1:length(message)
 end
 
 
-data
 for j = 1:length(alphabet)
     [m, a] = max(letterFreq);
     locations = strfind(message, alphabet(a));
-    dataCells{2, locations} = j;
+    for i = 1:length(locations)
+        dataCells{2, locations(i)} = j;
+    end
+    letterFreq(a) = 0;
 end
+
+for p = 1:length(message)
+    dataCells{3, p} = alphCommon(dataCells{2, p});
+end
+
+decodedM = char(dataCells{3, 1:end})';
+disp(decodedM);
